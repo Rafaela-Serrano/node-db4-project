7969fbb52c9ef593d748b472473e441df2195193
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-09-29 19:53:45 -0500

    wip

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const recipesRouter = require("../api/recipes/recipes-router")
+
+const server = express()
+
+server.use(express.json())
+
+server.use("/api/recipes", recipesRouter)
+
+module.exports = server 
