388f01f639bb610591175da8b0d80aea203ee124
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-10-02 10:19:06 -0500

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -2,8 +2,19 @@ const db = require('../../data/db-config')
 
 async function getRecipeById(recipe_id){
  const recipeRows = await db('recipes as r')
- .where('recipe_id', recipe_id)
+ .leftJoin('steps as s', 'r.recipe_id', 's.recipe_id')
+ .leftJoin('steps_ingredients as si','si.step_id','s.steps_id')
+ .select(
+    'r.recipe_id',
+    'r.recipe_name',
+    's.steps_id',
+    's.step_number',
+    's.step_text',
+ )
+ .where('r.recipe_id', recipe_id)
  return recipeRows
 }
+
+
 
 module.exports = {getRecipeById}
